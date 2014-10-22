282361003331c5280c9cd0ca8a9b442b7bcf8493
MeikeAi <m.aichele@gmx.de>
2014-10-22 15:04:45 +0800

    Bug fix in dragdrop innerHTML of content

--- src/js/dragdrop.js
+++ src/js/dragdrop.js
@@ -240,9 +240,8 @@
 		}else{
 
 			if(destinationObj){
-				//Change text to text with step field
-				console.log(contentToBeDragged.innerHTML);
-				contentToBeDragged.innerHTML += ": <input type='text' id='steps' value='1' size='5'/> (steps)"; 
+				// Change text to text with step field
+				contentToBeDragged.innerHTML = contentToBeDragged.id.substring(3) + ": <input type='text' id='steps' value='1' size='5'/> (steps)"; 
 				
 				if(destinationObj.tagName=='UL'){
 					destinationObj.appendChild(contentToBeDragged);
