8a6937a1fa53d9b2bac43cf1cf4c7dd2f90daa8d
auronsiow <quicklimebluessaliva@hotmail.com>
2014-10-31 16:52:12 +0800

    Add in sound effects
    New Interface Observer for listening to specific events
    Observers for Move, Jump, Hide, Show commands will play a sound effect.
    (To consider doing animations here instead??)

---
+++ src/js/observer.js
@@ -0,0 +1,67 @@
+//------------------------------------------------------------------------------------
+//
+//	Observer (Inteface) Class
+//
+//	Description: Interface for other data observers to implement the notify function
+//
+//------------------------------------------------------------------------------------
+var Observer = {
+	notify: function() {}
+}
+
+
+//------------------------------------------------------------------------------------
+//
+//	MoveCommandObserver (Implements Observer)
+//
+//	Description: Observer to listen for any execution of MoveCommand 
+//				 Plays 'Walking' sound effect when called upon
+//
+//------------------------------------------------------------------------------------
+var MoveCommandObserver = function() {
+	CommandProcessor.attachObserver(this);
+	this.audio = new Audio("audio/walk.m4a");
+}
+MoveCommandObserver.prototype = Object.create(Observer);
+MoveCommandObserver.prototype.notify = function(commandObject) { 
+	if(commandObject instanceof MoveCommand)
+		this.audio.play();
+}
+
+
+//------------------------------------------------------------------------------------
+//
+//	CommandProcessor (Implements Observer)
+//
+//	Description: Observer to listen for any execution of JumpCommand 
+//				 Plays 'Jumping' sound effect when called upon
+//
+//------------------------------------------------------------------------------------
+var JumpCommandObserver = function() {
+	CommandProcessor.attachObserver(this);
+	this.audio = new Audio("audio/jump.m4a");
+}
+JumpCommandObserver.prototype = Object.create(Observer);
+JumpCommandObserver.prototype.notify = function(commandObject) {    	
+	if(commandObject instanceof JumpCommand)
+		this.audio.play();	
+}
+
+
+//------------------------------------------------------------------------------------
+//
+//	ShowHideCommandObserver (Implements Observer)
+//
+//	Description: Observer to listen for any execution of Show or Hide Command 
+//				 Plays 'Popping' sound effect when called upon
+//
+//------------------------------------------------------------------------------------
+var ShowHideCommandObserver = function() {
+	CommandProcessor.attachObserver(this);
+	this.audio = new Audio("audio/gumpop.m4a");
+}
+ShowHideCommandObserver.prototype = Object.create(Observer);
+ShowHideCommandObserver.prototype.notify = function(commandObject) {
+	if(commandObject instanceof ShowCommand || commandObject instanceof HideCommand)
+		this.audio.play();	
+}
